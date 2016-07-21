5ee723a294d1d16a60f7aa39adf814e78e6ffa6a
ejimenezp <eduardo@cookingpoint.es>
2016-07-21 10:53:41 +0200

    remove social share
    removed scripts to share on twitter, fb and google. I have to review
    them with calm

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -27,37 +27,37 @@
 <a href="https://www.google.com/maps/place/Cooking+Point/@40.412387,-3.697495,15z/data=!4m5!3m4!1s0x0:0xbfa70f0e9ca1618!8m2!3d40.4123866!4d-3.6974954?hl=en" target="_blank"><img src="/images/plano-sidebar.png" /></a>
 
 </div>
-			<div class="row">
-				<div class="follow-us-en">
-					<ul>
-						<li><a href="https://twitter.com/share"
-							class="twitter-share-button" data-count="none">Tweet</a></li>
-						<li>
-							<div id="fb-root"></div>
-							<div class="fb-share-button" data-type="button"></div>
-						</li>
-						<li class="gplus">
-							<div class="g-plusone" data-size="medium" data-annotation="none"></div>
-						</li>
-					</ul>
-				</div>
-				<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 
-				<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=466532963479408";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
+{{-- <div class="row">
+	<div class="follow-us-en">
+		<ul>
+			<li><a href="https://twitter.com/share"
+				class="twitter-share-button" data-count="none">Tweet</a></li>
+			<li>
+				<div id="fb-root"></div>
+				<div class="fb-share-button" data-type="button"></div>
+			</li>
+			<li class="gplus">
+				<div class="g-plusone" data-size="medium" data-annotation="none"></div>
+			</li>
+		</ul>
+	</div>
+	<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 
-				<script type="text/javascript">
-  (function() {
-    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-    po.src = 'https://apis.google.com/js/platform.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-  })();
-</script>
-				</div>
-			</div>
+	<script>(function(d, s, id) {
+		  var js, fjs = d.getElementsByTagName(s)[0];
+		  if (d.getElementById(id)) return;
+		  js = d.createElement(s); js.id = id;
+		  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=466532963479408";
+		  fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));</script>
 
+	<script type="text/javascript">
+		  (function() {
+		    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+		    po.src = 'https://apis.google.com/js/platform.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+		  })();
+	</script>
+</div>
+ --}}
