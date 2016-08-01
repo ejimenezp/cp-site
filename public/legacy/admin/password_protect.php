06f218f0835c2406114455ead0490db8527d20d3
ejimenezp <eduardo@cookingpoint.es>
2016-08-01 11:55:11 +0200

    changed passwords
    only password now and lasts longer

--- public/legacy/admin/password_protect.php
+++ public/legacy/admin/password_protect.php
@@ -49,19 +49,18 @@ $LOGIN_INFORMATION = array(
 // Add login/password pairs below, like described above
 // NOTE: all rows except last must have comma "," at the end of line
 $LOGIN_INFORMATION = array(
-  'admin' => 'admin',
-  'Admin' => 'admin',
-
+  'Qq', 
+  'qq'
 );
 
 // request login? true - show login and password boxes, false - password box only
-define('USE_USERNAME', true);
+define('USE_USERNAME', false);
 
 // User will be redirected to this page after logout
 define('LOGOUT_URL', 'http://www.cookingpoint.es/');
 
 // time out after NN minutes of inactivity. Set to 0 to not timeout
-define('TIMEOUT_MINUTES', 5);
+define('TIMEOUT_MINUTES', 5000);
 
 // This parameter is only useful when TIMEOUT_MINUTES is not zero
 // true - timeout time from last activity, false - timeout time from login
