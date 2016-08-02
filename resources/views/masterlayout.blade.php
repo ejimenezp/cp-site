caa434be1f09148fc9d71c15034887a76c466323
ejimenezp <eduardo@cookingpoint.es>
2016-08-02 14:41:44 +0200

    include google analytics script
    universal.js

--- resources/views/masterlayout.blade.php
+++ resources/views/masterlayout.blade.php
@@ -18,6 +18,21 @@
       <link href="{{ elixir('css/app.css') }}" rel="stylesheet" type="text/css">     
       <script type='text/javascript' src='/js/app.js'></script>
       <script src="https://use.fontawesome.com/c502308363.js"></script>
+
+      @if (config('cookingpoint.env.app_env') == 'production')
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-43676257-1', 'auto');
+          ga('send', 'pageview');
+        </script>
+      @else
+        <!-- no analytics here. Testing environment -->
+      @endif
+
   </head>
     
   <body>
