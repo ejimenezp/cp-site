b4b8a05e87cc4529f791f8d5a1dc589688bc4bd4
ejimenezp <eduardo@cookingpoint.es>
2016-08-02 13:42:41 +0200

    add 404 page
    .

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,17 @@
+@extends('masterlayout')
+
+@section('title', 'Spanish Cooking Classes &amp; Wine Tastings in Madrid, Spain')
+@section('description', 'Spanish cooking classes and wine tastings everyday in Madrid. Cooking classes on paella and tapas in English in select culinary school in Spain')
+
+@section('content')
+
+
+<div class="row">
+ 
+
+<h1>Page Not Found</h1>
+
+<p>Sorry, but this page does not exist. Please select any option from the above menu.
+</p>
+</div>
+@stop
